572018d1955ba0004689b3fdaa0bdda37d6da271
Tan Phan <89547590+tphan3103@users.noreply.github.com>
2023-02-03 12:52:53 -0600

    Changed GitLab to use a helper class

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -3,6 +3,7 @@ package edu.unl.raikes.git;
 public class GitLab {
 
 	public static void main(String[] args) {
-		System.out.println("This is a lesson on git");
+		GitLabHelper helper = new GitLabHelper("Tan", "Emily", "Vaughn");
+		helper.PrintWelcomeMessage();
 	}
 }
